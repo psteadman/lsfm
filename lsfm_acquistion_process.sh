ffdcebd4815618d07bea281003a9f467c6e0e46e
psteadman <patrick.steadman@mail.utoronto.ca>
2017-05-15 10:01:44 -0400

    json header file included, python code should be all we need just needs work, example of what we currently do in the bash file

--- lsfm_acquistion_process.sh
+++ lsfm_acquistion_process.sh
@@ -2,7 +2,8 @@
 
 # code to stitch and combine L and R sheet from LSFM
 
-# Patrick Steadman 2016
+# Frankland lab 2017
+# Contributors: Patrick Steadman
 
 
 outputdir=$2
@@ -29,3 +30,33 @@ echo fiji --headless -macro ~/Dropbox/mbp/bin/combineSheets.ijm \
 # if combining is successful remove s0 and s1 stacks - maybe dont do this
 
 # is there a way to combine tif stack to hdf5 file format
+
+for folder in 17f-17*; do 
+
+echo "IMAGE" $folder; 
+cd $folder;
+echo "STARTING DIRECTORY" $PWD
+rename "s/ //g" s0/*
+rename "s/ //g" s1/*
+if [[ -d s1 ]] ; then
+echo s1 exists
+for file in s1/*_????.tif; do 
+dir=`dirname $file`; 
+base=`basename $file .tif`; 
+mv $file ${dir}/${base}_s1.tif
+done
+fi
+
+if [[ -d s0 ]] ; then
+echo s0 exists
+for file in s0/*_????.tif; do 
+dir=`dirname $file`; 
+base=`basename $file .tif`; 
+mv $file ${dir}/${base}_s0.tif
+done
+mkdir combined
+fiji --headless -macro ~/Dropbox/mbp/bin/combineSheets.ijm /media/psteadman/lightsheetZF1/LSFM_Data/Atlas/stitched_data/${folder}/s0/:/media/psteadman/lightsheetZF1/LSFM_Data/Atlas/stitched_data/${folder}/s1/:/media/psteadman/lightsheetZF1/LSFM_Data/Atlas/stitched_data/${folder}/combined/;
+fi
+
+cd -
+done
--- stitch_LSFMstack.py
+++ stitch_LSFMstack.py
@@ -3,51 +3,123 @@
 # code to stitch images and then combined sheets
 # given an input directory and imaging details
 
-# Patrick Steadman 2016
+# Frankland lab 2017
+# Contributors: Patrick Steadman
+
 import argparse
 import skimage
 import scipy.misc
 import os, sys
 import numpy
 import cv2
+import json
 
-def stitch_images(paramOverlap, paramArrangement, paramBlending, imgList):
+def stitch_images(img1, img2, paramOverlap, paramArrangement, paramBlending):
 	'''
-	This function input list of images on a single x-y plane of a z-stack
-	and stitch them according to a defined overlap and arrangement parameter
+	Stitches two images together
 
-	Blending of overlap region algorithm
-	- Linear blending: In the overlapping area the intensity 
+	paramOverlap:
+		percent of images that overlap
+	paramArrangement:
+		axis to blend in. 0 for x, 1 for y
+	paramBlending:
+	1 Linear blending: In the overlapping area the intensity 
 		are smoothly adjusted between the two images.
-		opencv cv2.AddWeighted() does this
-	- Average: In the overlapping area the average intensity 
+		opencv cv2.AddWeighted() 
+		see -> http://docs.opencv.org/2.4/modules/core/doc/operations_on_arrays.html
+	2 Average: In the overlapping area the average intensity 
 		between all images is computed (example source code).
-	- Median: In the overlapping area the median intensity of 
+	3 Median: In the overlapping area the median intensity of 
 		all images is computed.
-	- Max. Intensity: In the overlapping area the maximum 
+	4 Max. Intensity: In the overlapping area the maximum 
 		intensity between all images is used int the output image.
-	- Min. Intensity: In the overlapping area the minimum 
+	5 Min. Intensity: In the overlapping area the minimum 
 		intensity between all images is used int the output image.
 	'''
 
-	numpy.concatenate(img1,img2,0) # for x
-	numpy.concatenate(img1,img2,1) # for y
-	
+	# double check these tuples end up correct
+	# we assume first image then second image is order for stitch
+	img1_ol = ( img1.shape[paramArrangement] - img1.shape[paramArrangement]*paramOverlap, 
+				img1.shape[paramArrangement] )
+	img2_ol = ( 0, img2.shape[paramArrangement]*paramOverlap )
+
+	img1_top    = ( 0, img1.shape[paramArrangement] - img1.shape[paramArrangement]*paramOverlap )
+	img2_bottom = (img2.shape[paramArrangement]*paramOverlap, 
+				   img2.shape[paramArrangement] )
+
+	listParamBlending = ['blend','mean','median','max','min']
+	if paramBlending == listParamBlending[0]:
+		# blending param of 1.5 
+		img_ol = np.average(numpy.dstack(img1_ol,img2_ol),axis=2, 
+				weights=numpy.full(numpy.dstack((img1_ol,img2_ol)).shape, 1.5) )
+	elif paramBlending == listParamBlending[1]:
+		img_ol = np.mean(numpy.dstack((img1_ol,img2_ol)),2)
+	elif paramBlending == listParamBlending[2]:
+		img_ol = np.median(numpy.dstack((img1_ol,img2_ol)),2)
+	elif paramBlending == listParamBlending[3]:
+		img_ol = np.max(numpy.dstack((img1_ol,img2_ol)),2)
+	elif paramBlending == listParamBlending[4]:
+		img_ol = np.min(numpy.dstack((img1_ol,img2_ol)),2)
+	else:
+		print('incorrect blending options given, choose from: {}').format(listParamBlending)
+
+	# Put the images together
+	img_stitched = numpy.concatenate(img1_top, img_ol,, img2_bottom paramArrangement)
 
 
 if __name__ == "__main__":
 	description = """
-	Program to take light-sheet microscope z-stack and print image information
+	Input is directory containing images from LSFM
+	Output is directory containing z-stack of stitched and combined images
 	"""
 	parser = argparse.ArgumentParser(description=description)
-	parser.add_argument('-i','--input-directory',
+	parser.add_argument('-i','--input-directory',dest='inputdir',
 		help='LSFM raw stack directory')
-	parser.add_argument('imgs',nargs='+')
-	parser.add_argument('-o','--output-directory',help='output csv file')
+	parser.add_argument('-o','--output-directory',dest='outputdir'
+		help='output directory for z-stack, will create one for each channel')
+	parser.add_argument('-h','--header-json',dest='imageheader',
+		help='json file with image meta-data',default=None)
 	args = parser.parse_args()
 
-	# such a simple script
-	filename = os.path.basename(args[0])
-	img = scipy.ndimage.imread(args[0], flatten=True)  
-	scipy.misc.imsave(args[1],img)
+	filename = args.inputdir
+	if args.imageheader is not None:
+        with open(args.imageheader,'r') as fp:
+            imageHeader = json.load(fp)
+            stepsX = imageHeader["stepsX"]
+            stepsY = imageHeader["stepsY"]
+            stepsZ = imageheader["stepsZ"]
+            nChannels = imageheader["nChannels"]
+            nSheets = imageheader["nSheets"]
+            overlap = imageHeader["overlap"]
+    else:
+        print("Missing header json file")
+
+    # handle output directory
+    # create for each channel
+
+	for r in range(stepsZ):
+		# find files in that step
+
+		# separate by channel
+
+		# separate by light sheet
+
+		# read in images
+		filename = os.path.basename(args.imgs[r],'.tif') #helpful?
+		img = scipy.ndimage.imread(args.imgs[r], flatten=True) 
+		# confirm 16 bit with dtype
+
+		# stitch images together (and repeat for other sheet)
+		# for stepsX stitch
+		for x in range(stepsX-1):
+			# img1[x], img2[x+1]
+			img_x = stitch_images(img1, img2, overlap, 0, 0)
+		# for output and stepsY stitch
+
+		# merge sheets based on max intensities
+
+		# write stitched and merged z-stack[r] image to channel directory
+		# save with cv2 so 16 bit
+		out = args.outputdir+filename+'_'+str(r.zfill(4))+'.tif' 
+		print(out); scipy.misc.imsave(out,img)
 
